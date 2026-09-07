1350cac4ad64530c66d304fd32eded2b46442048
Samson <samsonbaraka32@gmail.com>
2026-09-07 14:53:09 +0300

    Add Task 5 printf c program of Hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- Defines the start of any program
+ *
+ * return always terminates the function of a program
+ */
+
+
+	int main(void)
+{
+
+	printf(" with proper grammar, but the outcome is a piece of art,\n");
+	/**
+	 * prints the output of the sentence
+	 *
+	 * return creates the output of the program
+	 */
+
+	return (0);
+
+}
