73e213bc3b7fa96089ec3b00935e00639f2cd1de
Samson <samsonbaraka32@gmail.com>
2026-09-07 21:04:44 +0300

    Add Task 6 print numberz C program without char variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
