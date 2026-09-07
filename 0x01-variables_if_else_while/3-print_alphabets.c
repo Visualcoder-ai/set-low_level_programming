75c61babecd5af22abfa213c465350df3328d7ca
Samson <samsonbaraka32@gmail.com>
2026-09-07 17:12:25 +0300

    Add Task 3 print alphabets C program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase then uppercase using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
