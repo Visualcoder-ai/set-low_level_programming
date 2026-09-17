0685fb6541dd609504726c57fd8fd5a96991ac99
Visualcoder-ai <workingstats746@gmail.com>
2026-09-17 12:40:02 +0300

    Task 8: Add jack_bauer function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
 
 #endif
