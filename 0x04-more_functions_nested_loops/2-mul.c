3ee986ecc16b0956bd0e252a66555d80b2d09680
Visualcoder-ai <workingstats746@gmail.com>
2026-09-20 23:10:40 +0300

    Add Task 2: mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers
+ *@a: first integer to multiply
+ *@b: Second integer to multiply
+ *
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+
+
