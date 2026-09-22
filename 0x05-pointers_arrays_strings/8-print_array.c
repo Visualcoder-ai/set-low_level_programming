e165e2e469ff0308d9ee4b7d462477ee86e6ab8c
Visualcoder-ai <workingstats746@gmail.com>
2026-09-23 01:33:06 +0300

    Fix line length Betty warning in task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
+ * print_array - Prints n elements of an array
+ *               of integers, followed by a new line.
  * @a: Pointer to the array of integers.
  * @n: Number of elements to be printed.
  *
@@ -22,3 +23,4 @@ void print_array(int *a, int n)
 	}
 	printf("\n");
 }
+
