8b1a783a1e1886476a5f7e3a435a00989ab74955
Visualcoder-ai <workingstats746@gmail.com>
2026-09-23 00:08:51 +0300

    Complete task 0: 98 Battery st and add README

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  *reset_to_98 - it takes a pointer to an integer and updates it
  *@n: pointer to the integer required to be updated.
  */
- void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
